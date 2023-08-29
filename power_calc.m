85902dc2dd89286da8d3dce1327ca9fbb0a7a32b
Jean-Michel Alvarez <jeanmichelaalvarez@gmail.com>
2023-08-29 16:19:58 +0100

    remove most files

--- power_calc.m
+++
@@ -1,8 +0,0 @@
-x = out.power{1}.Values.Time;
-y = out.power{1}.Values.Data;
-
-for i = 1:4
-    s(i) = sum(trapz(x,y(:,i)));
-end
-
-s_all = sum(s);
--- sim_time.m
+++
@@ -1,9 +0,0 @@
-for i = 1:20
-    tic
-    sim('model_quad_linear_mpc');
-    time(i) = toc;
-end
-mean(time)
-
-% 0.5264 for mpc N = 10, 0.58 for N = 50
-% 0.4635 for lqr N = 10, 0.4896 for N = 50                   
